ec9a4d92e85ffaec6670e3dc69993c1b19609590
Macmiller <macamagolf@gmail.com>
2022-01-17 00:26:39 -0300

    Arquivos redirecionados

--- Chapter2/Typescript/Criando_projeto_com_typescript/dist/CreateCourseService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateCourseService {
-    execute({ name, duration = 8, educator }) {
-        console.log(name, duration, educator);
-    }
-}
-exports.default = new CreateCourseService(); 
--- Chapter2/Typescript/Criando_projeto_com_typescript/src/server.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'; // Aqui está sendo importado o express
-import { createCourse } from './routes'; // Aqui está sendo importado a funçao (createCourse) do arquivo (routes.ts)
-
-const app = express();
-
-app.get("/", createCourse) // Aqui está sendo passado na rota a função (createCourse)
-
-app.listen(8080);
