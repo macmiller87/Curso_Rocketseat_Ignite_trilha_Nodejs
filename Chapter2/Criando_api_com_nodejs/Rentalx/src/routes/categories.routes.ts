8432466e1e9e5c7a9a7f5ada918171de27a11cd1
Macmiller <macamagolf@gmail.com>
2022-01-23 20:50:34 -0300

    Validacao da rota de criacao e import da rota e criacao da rota de listagem

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/categories.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/categories.routes.ts
@@ -1,21 +1,34 @@
-import { Router } from "express"; // Aqui está sendo importando a função (Router), para poder trabalhar com as rotas no server.ts
+import { Router } from "express"; // Aqui está sendo importando a função (Router), para poder trabalhar com as rotas no server.ts 
 
-const categoriesRoutes = Router(); //Aqui está sendo chamada a função (Router).
+import { CategoriesRepository } from "../repositories/categoriesRepository"; // Aqui está sendo importando a classe(CategoriesRepository).
 
-const categories = [];
-
+const categoriesRoutes = Router(); //Aqui está sendo chamada a função (Router). 
+const categoriesRepository = new CategoriesRepository(); // Aqui está chamada a classe (CategoriesRepository);
 
 // Rota para criar o nome e a categoria do carro.
-categoriesRoutes.post("/categories", (req,res) => {
+categoriesRoutes.post("/", (req,res) => {
     const { name, description } = req.body;
 
-    categories.push({
-        name,
-        description
-    })
+    const categoryAlredyExists = categoriesRepository.findByName(name); // Aqui está sendo setado a classe (categoriesRepository), com a função (findByName(name)), que está tratando a validação no arquivo categoriesRepository.ts
+
+    if(categoryAlredyExists) {
+        return res.status(400).json({error: "Category Alredy exists !"});
+    }
+
+    categoriesRepository.create({ name, description }); //  Aqui está sendo setado a classe (CategoriesRepository) e passando a função create({name, description}) com seus atributos name e description.
 
     return res.status(201).send();
 });
 
-export { categoriesRoutes }; // Aqui está eportando o método acima
+
+// Rota para listar as informações do carro.
+categoriesRoutes.get("/", (req, res) => {
+
+    const all = categoriesRepository.list(); // Aqui está sendo usado a classe (categoriesRepository) e a função list(), para poder fazer a listagem dos items.
+
+    return res.json(all);
+});
+
+
+export { categoriesRoutes }; // Aqui está exportando o método acima.
 
--- Chapter2/Criando_api_com_nodejs/Rentalx/src/server.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/server.ts
@@ -4,7 +4,7 @@ import { categoriesRoutes } from "./routes/categories.routes"; // Aqui está sen
 const app = express();
 
 app.use(express.json()); // Aqui está sendo setado o express.json, para poder usar.
-app.use(categoriesRoutes); // Aqui está sendo setado o categoriesRoutes, para poder usar as rotas.
+app.use("/categories", categoriesRoutes); // Aqui está sendo setado o categoriesRoutes, e foi adicionado um patch => ("/categories"), para ficar como padrão dessa função, e não precisar adicionar na rota no arquivo (categories.routes), apenas adiciona o ("/"), para poder usar essa rota.
 
 app.listen(8080, () => console.log('Server is running!'));
 
