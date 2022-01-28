4523998255fb55207b73c57f435f5be1045f8b7a
Macmiller <macamagolf@gmail.com>
2022-01-28 03:32:56 -0300

    Atualizando as rotas

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/categories.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/categories.routes.ts
@@ -1,38 +1,20 @@
 import { Router } from "express"; // Aqui está sendo importando a função (Router), para poder trabalhar com as rotas no server.ts 
 
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService"; // Aqui está sendo importando a classe(CreateCategoryService).
-
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository"; // Aqui está sendo importando a classe(CategoriesRepository).
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router(); //Aqui está sendo chamada a função (Router). 
-const categoriesRepository = new CategoriesRepository(); // Aqui está chamada a classe (CategoriesRepository);
 
 // Rota para criar o nome e a categoria do carro.
 categoriesRoutes.post("/", (req, res) => {
-    const { name, description } = req.body;
-
-    const createCategoryService = new CreateCategoryService(categoriesRepository); // Aqui foi criado um objeto e instanciado e passado o (categoriesRepository).
-
-    createCategoryService.execute({name, description}); // Aqui está sendo chamado o método execute() ad classe (CreateCategoryService) da pasta services.
-
-    // const categoryAlredyExists = categoriesRepository.findByName(name); // Aqui está sendo setado a classe (categoriesRepository), com a função (findByName(name)), que está tratando a validação no arquivo categoriesRepository.ts
-
-    // if(categoryAlredyExists) {
-    //     return res.status(400).json({error: "Category Alredy exists !"});
-    // }
-
-    // categoriesRepository.create({ name, description }); //  Aqui está sendo setado a classe (CategoriesRepository) e passando a função create({name, description}) com seus atributos name e description.
-
-    return res.status(201).send();
+    return createCategoryController.handle(req, res); // Aqui está sendo retornado o (createCategoryController), com a função handle(req, res) com os parametros.
 });
 
 
 // Rota para listar as informações do carro.
 categoriesRoutes.get("/", (req, res) => {
+    return listCategoriesController.handle(req, res);
 
-    const all = categoriesRepository.list(); // Aqui está sendo usado a classe (categoriesRepository) e a função list(), para poder fazer a listagem dos items.
-
-    return res.json(all);
 });
 
 
