6a923a5a9bdf9164b5f6bc7d5f78c3a49a390628
Macmiller <macamagolf@gmail.com>
2022-02-02 04:26:37 -0300

    Conceito de stream

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,19 @@
+import { parse as csvParse } from "csv-parse"; // Aqui está sendo importado o (parse), e foi renomeado para (csv-parse);
+
+import fs from "fs"; // Aqui está sendo instalado a lib (FS), que já é nativa do Nodejs, sem a necessidade de instalar, essa lib é para leitura de arquivos.
+
 class ImportCategoryUseCase {
-    execute(file: any) { // Aqui foi criado a função (execute) assando o (file: any) como parametro.
-        console.log(file);
+    execute(file: Express.Multer.File): void { // Aqui foi criado a função (execute) assando o (file: Express.Multer.File) como parametro.
+
+        const stream = fs.createReadStream(file.path); // Aqui está sendo passado a função (createReadStream(file.path)) padrão para a leitura de arquivos.
+
+        const parseFile = csvParse(); // Aqui está sendo setado a funçaõ (csvParse()), que é responsável por ler linha por linha do arquivo repassado. 
+
+        stream.pipe(parseFile); // Aqui está sendo usado a função (pipe), que recebe cada pedaço da leitura do arquivo através do (stream).
+
+        parseFile.on("data", async (line) => {
+            console.log(line);
+        });
     }
 }
 
