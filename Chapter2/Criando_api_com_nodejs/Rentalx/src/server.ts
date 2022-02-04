6ae522302e179307898b73d4e8dd142cc3e99168
Macmiller <macamagolf@gmail.com>
2022-02-04 02:50:46 -0300

    Configurando o swagger

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/server.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/server.ts
@@ -1,8 +1,13 @@
 import express from "express";
+import swaggerUi  from "swagger-ui-express"; // Aqui está sendo importado o Swagger.
 import { router } from "./routes";
+import swaggerFile from "./swagger.json"; // Para este import precisa colocar no arquivo (tsconfig.json), a opção ("resolveJsonModule": true) desse jeito.
 
 const app = express();
+
 app.use(express.json()); // Aqui está sendo setado o express.json, para poder usar.
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile)); // Aqui está sendo setado os parametros padrão para utilização so swwager.
 
 app.use(router); // Aqui foi setado o (router), que está no arquivo index.ts de rotas da aplicação. 
 
