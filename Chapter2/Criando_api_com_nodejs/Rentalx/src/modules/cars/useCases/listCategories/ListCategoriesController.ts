8ddbd926c637e0326b923d990e6826e50f933db1
Macmiller <macamagolf@gmail.com>
2022-02-23 00:24:26 -0300

    Refatorando os usecases de listcategories

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express";
+import { container } from "tsyringe";
 import { ListCategoryUseCase } from './ListCategoriesUseCase';
 
 class ListCategoriesController {
-    constructor(private listCategoriesUseCase: ListCategoryUseCase) {}
 
-    handle(req: Request, res: Response): Response {
+   async handle(req: Request, res: Response): Promise<Response> {
 
-        const all = this.listCategoriesUseCase.execute(); // Aqui está sendo usado a classe (categoriesRepository) e a função list(), para poder fazer a listagem dos items.
+        const listCategoriesUseCase = container.resolve(ListCategoryUseCase);
+
+        const all = await listCategoriesUseCase.execute(); // Aqui está sendo usado a classe (categoriesRepository) e a função list(), para poder fazer a listagem dos items.
 
         return res.json(all);
     }
--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,13 +1,15 @@
+import { inject, injectable } from 'tsyringe';
 import { Category } from '../../entities/category';
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
 
+@injectable()
 class ListCategoryUseCase {
-    constructor(private categoriesRepository: ICategoriesRepository) {} // Aqui foi criado o (constructor) e passado como (private), chamando (CategoriesRepository)
+    constructor(@inject("CategoriesRepository") private categoriesRepository: ICategoriesRepository) {} // Aqui foi criado o (constructor) e passado como (private), chamando (CategoriesRepository)
 
     // Aqui foi criado a função execute passando os parametros e setando a interface (IRequest), e dizendo que essa função e (void).
-    execute(): Category[] {
+    async execute(): Promise<Category[]> {
 
-        const categories = this.categoriesRepository.list();
+        const categories = await this.categoriesRepository.list();
 
         return categories;
     }
