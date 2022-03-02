f22eb4a68ec4008a7eff52975ab82ec6b3bc89a6
Macmiller <macamagolf@gmail.com>
2022-03-02 00:06:48 -0300

    Atualizando rota

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/specification.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/specification.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 
@@ -6,6 +7,8 @@ const specificationsRoutes = Router(); // Aqui está sendo setado o (Router) do 
 
 // Aqui foi criado essa const, para referenciar a classe (createSpecificationController) e suas dependências, para poder ser usada na rota, como se fosse uma função middleware.
 const createSpecificationController = new CreateSpecificationController();
+
+specificationsRoutes.use(ensureAuthenticated);
 
 // Aqui está sendo criada a rota de criação da specificação da categoria do carro, passando a classe (createSpecificationController) e suas dependências, com a função handle.
 specificationsRoutes.post("/", createSpecificationController.handle);
