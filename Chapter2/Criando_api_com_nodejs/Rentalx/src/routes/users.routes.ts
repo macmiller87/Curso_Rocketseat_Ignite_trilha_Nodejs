b49fea19a5c6ae2c8ee5a1196d7d837cb330532f
Macmiller <macamagolf@gmail.com>
2022-03-04 00:02:09 -0300

    Criacao da rota avatar do usuario

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/users.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/users.routes.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
 
 const createUserController = new CreateUserController();
+const updateUserAvatarController = new UpdateUserAvatarController();
 
+// Rota de criação do usuário
 usersRoutes.post("/", createUserController.handle);
+
+// Rota de criação e atualização do avatart do usuário
+usersRoutes.patch("/avatar", updateUserAvatarController.handle);
 
 export { usersRoutes };
