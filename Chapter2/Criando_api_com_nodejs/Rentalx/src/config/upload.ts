21b20bdcf1d52fe6b6e38d37b357ac2c460b03e5
Macmiller <macamagolf@gmail.com>
2022-03-05 15:13:29 -0300

    Criacao da classe para fazer tratamento do upload do avatar

---
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/config/upload.ts
@@ -0,0 +1,21 @@
+import crypto from "crypto";
+import multer from "multer";
+import { resolve } from "path";
+
+
+export default {
+
+    upload(folder: string) {
+        return {
+            storage: multer.diskStorage({
+                destination: resolve(__dirname, "..", "..", folder),
+                filename: (request, File, callback) => {
+                   const fileHash = crypto.randomBytes(16).toString("hex");
+                   const fileName = `${fileHash}-${File.originalname}`;
+
+                   return callback(null, fileName);
+                },
+            }),
+        };
+    },
+};
