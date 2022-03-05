56b12a00d0b4e2d272fe6adafba79eb594abd163
Macmiller <macamagolf@gmail.com>
2022-03-05 15:10:51 -0300

    Criacao da rota para upload do avatar do usuario

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/users.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
 const usersRoutes = Router();
+
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar")); // Essa const está recebendo, o import da função de (uploadConfig) e a localização da pasta e arquivo (TMP/avatar).
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
@@ -11,6 +16,6 @@ const updateUserAvatarController = new UpdateUserAvatarController();
 usersRoutes.post("/", createUserController.handle);
 
 // Rota de criação e atualização do avatar do usuário
-usersRoutes.patch("/avatar", updateUserAvatarController.handle);
+usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle);
 
 export { usersRoutes };
