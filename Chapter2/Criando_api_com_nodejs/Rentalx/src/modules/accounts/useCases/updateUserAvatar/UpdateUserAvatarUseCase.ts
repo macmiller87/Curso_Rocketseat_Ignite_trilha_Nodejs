09c511eee586a935dd77b840f7bad82d6e62423a
Macmiller <macamagolf@gmail.com>
2022-03-06 15:19:13 -0300

    Atualizacao caso de uso para verificacao de avatar

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,4 +1,5 @@
 import { inject, injectable } from "tsyringe";
+import { deleteFile } from "../../../../utils/file";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -12,6 +13,11 @@ class UpdateUserAvatarUseCase {
 
     async execute({ user_id, avatar_File }: IRequest): Promise<void> {
         const user =  await this.usersRepository.findById(user_id);
+
+        if(user.avatar) {
+            
+            await deleteFile(`./tmp/avatar/${user.avatar}`); // Aqui está chamando a função (deleteFile), que tem a função de verificar se o arquivo de upload já existe, se der falso, então o fluxo aqui segue, (obs: esse cara aqui (./tmp/avatar/) é o endereço da pasta onde está sendo salvo o avatar)
+        }
 
         // Aqui está refeenciando a coluna da tabela (users)
         user.avatar = avatar_File;
