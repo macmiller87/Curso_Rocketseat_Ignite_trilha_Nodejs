f12b0bdb5f9adc6daaae6235863bc74349545a1a
Macmiller <macamagolf@gmail.com>
2022-03-06 15:25:05 -0300

    Logica para a verificacao e exclusao de avatar ja existente

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/utils/file.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/utils/file.ts
@@ -1,6 +1,6 @@
 import fs from "fs"; // Módulo interno do próprio Nodejs
 
-// Essa cons é responsavel pela verificação do qruivo de upload, se ele existe ou não.
+// Essa cons é responsavel pela verificação do arquivo de upload, se ele existe ou não.
 export const deleteFile = async(filename: string) => {
 
   try {
