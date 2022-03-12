1eb7e4096d0e95ecee9141d94d636a7e2dad21b7
Macmiller <macamagolf@gmail.com>
2022-03-12 00:07:52 -0300

    Repositorio in memory createcategory teste jest

---
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -0,0 +1,30 @@
+import { Category } from "../../entities/category";
+import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
+
+
+class CategoryRepositoryInMemory implements ICategoriesRepository {
+
+    categories: Category[] = [];
+
+    async findByName(name: string): Promise<Category> {
+        const category = this.categories.find((category) => category.name === name);
+        return category;
+    }
+
+    async list(): Promise<Category[]> {
+        const all = this.categories;
+        return all;
+    }
+
+    async create({ name, description }: ICreateCategoryDTO): Promise<void> {
+        const category = new Category();
+
+        Object.assign(category, { name, description});
+
+        this.categories.push(category);
+
+    }
+
+}
+
+export { CategoryRepositoryInMemory };
