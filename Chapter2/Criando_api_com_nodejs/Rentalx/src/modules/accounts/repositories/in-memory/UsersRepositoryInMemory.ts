4826b10ebfd6f7a2a4acc74c65a82707e729d6e6
Macmiller <macamagolf@gmail.com>
2022-03-13 17:01:44 -0300

    Repositorio in memory createuser teste jest

---
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/accounts/repositories/in-memory/UsersRepositoryInMemory.ts
@@ -0,0 +1,35 @@
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+
+class UsersRepositoryInMemory implements IUsersRepository {
+
+    Users: User[] = [];
+
+    async create({ driver_license, email, name, password }: ICreateUserDTO): Promise<void> {
+        const user =  new User();
+
+        Object.assign(user, { 
+            driver_license, 
+            email, 
+            name, 
+            password 
+        });
+
+        this.Users.push(user);
+        
+    }
+
+    async findByEmail(email: string): Promise<User> {
+        const user =  this.Users.find((user) => user.email === email);
+        return user;
+    }
+
+    async findById(id: string): Promise<User> {
+        const user = this.Users.find((user) => user.id === id);
+        return user;
+    }
+}
+
+export { UsersRepositoryInMemory };
