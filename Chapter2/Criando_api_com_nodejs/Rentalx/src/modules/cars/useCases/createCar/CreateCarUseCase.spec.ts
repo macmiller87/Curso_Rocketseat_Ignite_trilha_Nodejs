41abca4fdf11323a077940cfd98bba8442cab354
Macmiller <macamagolf@gmail.com>
2022-03-20 15:58:42 -0300

    Criando teste de verificacao do carro pela placa e verificacao da disponibilidade do carro

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/createCar/CreateCarUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
 let createCarUseCase:  CreateCarUseCase;
@@ -11,9 +12,10 @@ describe("Create Car", () => {
         createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
     });
 
+    // Teste que cria o carro.
     it("Should be able a create a new car", async () => {
 
-        await createCarUseCase.execute({
+      const car = await createCarUseCase.execute({
             name: "Name Car", 
             description: "Description Car",
             daily_rate: 100,
@@ -23,6 +25,49 @@ describe("Create Car", () => {
             category_id: "Category"  
         });
 
+        expect(car).toHaveProperty("id");
     });
+
+    // Teste que verifica se o carro em criação, possui a mesma placa.
+    it("Should not be able to create a car, if already exists a  car with same license plate!", () => {
+
+        expect(async () => {
+            await createCarUseCase.execute({
+                name: "Car 1", 
+                description: "Description Car",
+                daily_rate: 100,
+                license_plate: "ABC-1234",
+                fine_amount: 60,
+                brand: "Brand",
+                category_id: "Category"  
+            });
+
+            await createCarUseCase.execute({
+                name: "Car 2", 
+                description: "Description Car",
+                daily_rate: 100,
+                license_plate: "ABC-1234",
+                fine_amount: 60,
+                brand: "Brand",
+                category_id: "Category"  
+            });  
+        }).rejects.toBeInstanceOf(AppError);
+    }); 
+
+    // Teste que verifica se pode criar um carro com (available/disponibilidade) por (padrão === true).
+    it("Should be able to create a car with true availability by default!", async () => {
+
+        const car = await createCarUseCase.execute({
+            name: "Car Available", 
+            description: "Description Car",
+            daily_rate: 100,
+            license_plate: "ABCD-1234",
+            fine_amount: 60,
+            brand: "Brand",
+            category_id: "Category"  
+        }); 
+
+        expect(car.available).toBe(true);
+    }); 
 
 });
