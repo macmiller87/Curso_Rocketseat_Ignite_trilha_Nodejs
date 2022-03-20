6c8e929afc28c11982d4c7b8b2fdb353debe4903
Macmiller <macamagolf@gmail.com>
2022-03-20 15:48:28 -0300

    Criando a funcao findByLicensePlate

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/repositories/ICarsRepository.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/repositories/ICarsRepository.ts
@@ -1,9 +1,11 @@
 import { ICreateCarDTO } from "../dtos/ICreateCarDTO";
+import { Car } from "../infra/typeorm/entities/Car";
 
 
 interface ICarsRepository {
 
-    create(date: ICreateCarDTO): Promise<void>;
+    create(date: ICreateCarDTO): Promise<Car>;
+    findByLicensePlate(license_plate: string): Promise<Car>
 }
 
 export { ICarsRepository };
