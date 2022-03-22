2be5c57eea36c911dca212032bf3b9757c6d69a4
Macmiller <macamagolf@gmail.com>
2022-03-22 00:13:10 -0300

    Estruturando a entidade de cars

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/infra/typeorm/entities/Car.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,31 +1,49 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryColumn } from "typeorm";
 import { v4 as uuidV4 } from "uuid";
+import { Category } from "./category";
+
+@Entity("cars")
 class Car {
+
+   @PrimaryColumn()
    id: string;
 
+   @Column()
    name: string;
 
+   @Column()
    description: string;
 
+   @Column()
    daily_rate: number;
 
+   @Column()
    available: boolean;
 
+   @Column()
    license_plate: string
 
+   @Column()
    fine_amount: number;
 
+   @Column()
    brand: string;
 
+   // Aqui foi criado esse (category), para unir a coluna abaixo a (category_id)e também a refêrencia com a tabela (categories).
+   @ManyToOne(() => Category)
+   @JoinColumn({ name: "category_id"})
+   category: Category;
+
+   @Column()
    category_id: string;
 
+   @CreateDateColumn()
    created_at: Date
 
-   // Se o (id) for diferente, esse construtor está sentando o uuid, e os metodos abaixo. 
    constructor() {
       if(!this.id) {
          this.id = uuidV4();
          this.available = true;
-         this.created_at = new Date();
       }
    }
 }
