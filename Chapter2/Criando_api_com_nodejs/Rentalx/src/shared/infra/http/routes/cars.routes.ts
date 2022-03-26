31da65ca2980afedfb7238b18fdbe94f5816f36b
Macmiller <macamagolf@gmail.com>
2022-03-26 00:04:35 -0300

    Criando a rota de listagem de cars

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/cars.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
+import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { Router } from "express";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
@@ -6,8 +7,12 @@ import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCarsController = new ListAvailableCarsController();
 
-// Aqui nessa rota está sendo passado as duas funções middlewares para verificar se o usuário está autenticado e se é administrador.
+// Rota de criação do carro, aqui nessa rota está sendo passado as duas funções middlewares para verificar se o usuário está autenticado e se é administrador.
 carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle);
+
+// Rota de listagem de carros por (category_id, brand, name)
+carsRoutes.get("/available", listAvailableCarsController.handle);
 
 export { carsRoutes };
