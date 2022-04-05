a98360501a06bdb7c0677962ddf8e38431d4df9b
Macmiller <macamagolf@gmail.com>
2022-04-05 00:21:46 -0300

    Atualizando comentario na rota

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/authenticate.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/authenticate.routes.ts
@@ -5,6 +5,7 @@ const authenticateRoutes =  Router();
 
 const authenticateUserController =  new AuthenticateUserController();
 
+// Rota de autenticação do usuário.
 authenticateRoutes.post("/sessions", authenticateUserController.handle);
 
 export { authenticateRoutes };
