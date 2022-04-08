85d914df4f45f5812abd2b698c4d6edd810d68a6
Macmiller <macamagolf@gmail.com>
2022-04-08 00:00:27 -0300

    Continuacao dos testes de integracao de createcategorycontroller

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,14 +1,58 @@
-import { app } from "@shared/infra/http/app";
+import { hash } from "bcryptjs";
 import request from "supertest";
+import { Connection } from "typeorm";
+import { v4 as uuid } from "uuid";
+import { app } from "@shared/infra/http/app";
+import createConnection from "@shared/infra/typeorm/index";
 
-describe("Create Category Controller", async () => {
+// Essa let recebe a função (Connection) do typeorm.
+let connection: Connection;
+
+describe("Create Category Controller", () => {
+
+    beforeAll(async () => {
+        // Aqui espera a conexão com o BD estabelecer.
+        connection = await createConnection();
+
+        // Aqui após conectar o bd roda as (migrations).
+        await connection.runMigrations();
+
+        const id = uuid();
+        const password = await hash("admin", 8);
+
+        // Cria o usuário (ADM).
+        await connection.query(
+            `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license)
+            values('${id}', 'Admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'xxxxxx')`
+        );
+    });
+
+    afterAll(async () => {
+        // Aqui após os testes exclui a tabela
+        await connection.dropDatabase();
+
+        // Fecha a conexão com o BD.
+        await connection.close();
+    });
 
     it("Should be able to create a new category", async () => {
 
+        // Cria o usuário ADM na rota(sessions).
+        const responseToken = await request(app).post("/sessions").send({
+            email: "admin@rentx.com.br",
+            password: "admin"
+        });
+
+        // Aqui recupera o token do usuário.
+        const { token } =  responseToken.body;
+
+        // Cria =a categoria na rota(categories).
         const response = await request(app).post("/categories").send({
             name: "Category Supertest",
             description: "Category Supertest"
-        });
+        }).set({
+            Authorization: `Bearer ${token}` // Verifica se o usuário é ADM pelo (token).
+        })
 
         expect(response.status).toBe(201);
     });
