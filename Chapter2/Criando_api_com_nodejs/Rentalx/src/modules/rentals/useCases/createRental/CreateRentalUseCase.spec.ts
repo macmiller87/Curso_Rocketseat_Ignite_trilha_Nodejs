81fc1c133920814ff46d46db7ea0d7d5e76ce1cd
Macmiller <macamagolf@gmail.com>
2022-04-17 16:33:48 -0300

    Adicionando carsRepositoryInMemory

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -3,9 +3,11 @@ import dayjs from "dayjs";
 import { RentalsRepositoryInMemory } from "../../repositories/in-memory/RentalsRepositoryInMemory";
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
 
 let createRentalUsecase: CreateRentalUseCase;
 let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 let dayJsDateProvider: DayjsDateProvider;
 
 describe("Create Rental", () => {
@@ -13,8 +15,9 @@ describe("Create Rental", () => {
 
     beforeEach(() => {
         rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+        carsRepositoryInMemory = new CarsRepositoryInMemory();
         dayJsDateProvider = new DayjsDateProvider();
-        createRentalUsecase = new CreateRentalUseCase(rentalsRepositoryInMemory, dayJsDateProvider);
+        createRentalUsecase = new CreateRentalUseCase(rentalsRepositoryInMemory, dayJsDateProvider, carsRepositoryInMemory);
     });
 
     // Testa se é capaz de criar um novo aluguel do carro.
