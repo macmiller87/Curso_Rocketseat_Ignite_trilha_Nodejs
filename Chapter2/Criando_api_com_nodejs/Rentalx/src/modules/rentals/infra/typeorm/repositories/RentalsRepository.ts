1d9f7e19b283b65d907e99c3364c53a1b6678890
Macmiller <macamagolf@gmail.com>
2022-04-19 00:01:57 -0300

    Adicionando parametros para aluguel do carro

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -12,23 +12,37 @@ class RentalsRepository implements IRentalsRepository {
     }
 
     async findOpenRentalByCar(car_id: string): Promise<Rental> {
-        const openByCar = await this.repository.findOne({ car_id });
+        const openByCar = await this.repository.findOne({
+            where: { car_id, end_date: null }
+        });
+
         return openByCar;
     }
 
     async findOpenRentalByUser(user_id: string): Promise<Rental> {
-        const openByUser = await this.repository.findOne({ user_id });
+        const openByUser = await this.repository.findOne({
+            where: { user_id, end_date: null }
+        });
+
         return openByUser;
     }
 
-    async create({ car_id, user_id, expected_return_date }: ICreateRentalDTO): Promise<Rental> {
+    async create({ car_id, user_id, expected_return_date, id, end_date, total }: ICreateRentalDTO): Promise<Rental> {
         const rental = this.repository.create({ 
             car_id,
             user_id,
-            expected_return_date
+            expected_return_date,
+            id,
+            end_date,
+            total
         });
 
         await this.repository.save(rental);
+        return rental;
+    }
+
+    async findById(id: string): Promise<Rental> {
+        const rental = await this.repository.findOne(id);
         return rental;
     }
 }
