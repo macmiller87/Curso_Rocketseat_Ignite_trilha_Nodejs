00af5d5f88658f1d8bf184c612e856de4b219dba
Macmiller <macamagolf@gmail.com>
2022-04-19 00:10:11 -0300

    Criando e inplementando funcao compareInDays

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/DateProvider/IDateProvider.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/DateProvider/IDateProvider.ts
@@ -2,6 +2,7 @@ interface IDateProvider {
     compareInHours(start_date: Date, end_date: Date): number;
     convertToUtc(date: Date): string;
     dateNow(): Date;
+    compareInDays(start_date: Date, end_date: Date): number;
 }
 
 export { IDateProvider };
--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/DateProvider/implementations/DayjsDateProvider.ts
@@ -7,6 +7,7 @@ dayjs.extend(utc);
 
 class DayjsDateProvider implements IDateProvider {
  
+    // Faz a comparação em horas.
     compareInHours(start_date: Date, end_date: Date): number {
         
         const end_date_utc = this.convertToUtc(end_date);
@@ -21,6 +22,14 @@ class DayjsDateProvider implements IDateProvider {
 
     dateNow(): Date {
         return dayjs().toDate();
+    }
+
+    // Faz a comparação em dias.
+    compareInDays(start_date: Date, end_date: Date): number {
+        const end_date_utc = this.convertToUtc(end_date);
+        const start_date_utc = this.convertToUtc(start_date); 
+
+        return dayjs(end_date_utc).diff(start_date_utc, "days");
     }
 }
 
