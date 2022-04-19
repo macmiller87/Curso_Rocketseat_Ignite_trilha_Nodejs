40f7597ba7de61721cb4c9e6411b1a1d81fb2ce1
Macmiller <macamagolf@gmail.com>
2022-04-19 00:15:35 -0300

    Criando rota de devolutionRental

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/rental.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/rental.routes.ts
@@ -1,13 +1,18 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const rentalRoutes = Router();
 
 const createRentalControler = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 // Rota de criação do aluguel, com usuário autenticado.
 rentalRoutes.post("/", ensureAuthenticated, createRentalControler.handle);
+
+// Rota que faz a devolução do carro.
+rentalRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle);
 
 export { rentalRoutes };
 
