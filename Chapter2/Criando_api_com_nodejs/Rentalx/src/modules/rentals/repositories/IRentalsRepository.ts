868b890f2d5a384056b919e0871dd7c556ea17f5
Macmiller <macamagolf@gmail.com>
2022-04-20 00:04:28 -0300

    Adicionando parametros que relacionan rental e car

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/rentals/repositories/IRentalsRepository.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/rentals/repositories/IRentalsRepository.ts
@@ -1,5 +1,6 @@
 import { ICreateRentalDTO } from "../dtos/ICreateRentalDTO";
 import { Rental } from "../infra/typeorm/entities/Rental";
+
 interface IRentalsRepository {
     findOpenRentalByCar(car_id: string): Promise<Rental>;
     findOpenRentalByUser(user_id: string): Promise<Rental>;
