b75172b6ddd65a7a43b9e60b2d979ab2c9be136f
Macmiller <macamagolf@gmail.com>
2022-04-20 00:07:48 -0300

    Criando rota de listagem de aluguel

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/rental.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/rental.routes.ts
@@ -1,5 +1,6 @@
 import { CreateRentalController } from "@modules/rentals/useCases/createRental/CreateRentalController";
 import { DevolutionRentalController } from "@modules/rentals/useCases/devolutionRental/DevolutionRentalController";
+import { ListRentalsByUserController } from "@modules/rentals/useCases/listRentalsByUser/ListRentalsByUserController";
 import { Router } from "express";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
@@ -7,12 +8,16 @@ const rentalRoutes = Router();
 
 const createRentalControler = new CreateRentalController();
 const devolutionRentalController = new DevolutionRentalController();
+const listRentalsByUserController = new ListRentalsByUserController();
 
 // Rota de criação do aluguel, com usuário autenticado.
 rentalRoutes.post("/", ensureAuthenticated, createRentalControler.handle);
 
-// Rota que faz a devolução do carro.
+// Rota que faz a devolução do carro, com usuário autenticado.
 rentalRoutes.post("/devolution/:id", ensureAuthenticated, devolutionRentalController.handle);
+
+// Rota de listagem dos alugueis, com usuário autenticado.
+rentalRoutes.get("/user", ensureAuthenticated, listRentalsByUserController.handle);
 
 export { rentalRoutes };
 
