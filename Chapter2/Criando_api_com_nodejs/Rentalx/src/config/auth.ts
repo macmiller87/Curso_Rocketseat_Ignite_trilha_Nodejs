d8322b1c74ab521b7fd8518b3efddd0dbb47d2d8
Macmiller <macamagolf@gmail.com>
2022-04-24 15:15:28 -0300

    Criando funcao com parametros para o token

---
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+    secret_token: "7c3ef5a44d0cae90dc699db22182a57c",
+    expires_in_token: "15m",
+    secret_refresh_token: "c76aebd1642c40b12b0f5e50d9bbf1dc",
+    expires_in_refresh_token: "30d",
+    expires_refresh_token_days: 30
+};
