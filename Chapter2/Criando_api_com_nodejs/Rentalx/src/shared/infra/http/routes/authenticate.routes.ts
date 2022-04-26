4bbd0385557b6686fa44afc6d319ad4217a09789
Macmiller <macamagolf@gmail.com>
2022-04-26 00:06:06 -0300

    Criando rota de refreshtoken

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/authenticate.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "@modules/accounts/useCases/authenticateUser/AuthenticateUserController";
+import { RefreshTokenController } from "@modules/accounts/useCases/refreshToken/RefreshTokenController";
 
 const authenticateRoutes =  Router();
 
 const authenticateUserController =  new AuthenticateUserController();
+const refreshTokenController =  new RefreshTokenController();
 
 // Rota de autenticação do usuário.
 authenticateRoutes.post("/sessions", authenticateUserController.handle);
+
+// Rota de refreshToken.
+authenticateRoutes.post("/refresh-token", refreshTokenController.handle);
 
 export { authenticateRoutes };
