4b5003815f2775688a82dcdd9ececc0188e2485c
Macmiller <macamagolf@gmail.com>
2022-04-27 00:07:04 -0300

    Criando container de EtherealMailProvider

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/index.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe";
 import { IDateProvider } from "./DateProvider/IDateProvider";
+import { IMailProvider } from "./MailProvider/IMailProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
+import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
     DayjsDateProvider
-)
+);
+
+container.registerInstance<IMailProvider>(
+    "EtherealMailProvider",
+    new EtherealMailProvider() // Foi necessário usar o (registerInstance) e instância o (EtherealMailProvider()), para poder conseguir fazer a injeção de dependência no usecase.
+);
