66a5d3723d49ae9c8226558366d0fdba57851b80
Macmiller <macamagolf@gmail.com>
2022-04-29 00:07:45 -0300

    Criando rota de reset de senha do usuario

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/password.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,16 @@
+import { ResetPasswordUserController } from "@modules/accounts/useCases/resetPasswordUser/ResetPasswordUserController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPasswordMail/SendForgotPasswordMailController";
 import { Router } from "express";
 
 const passwordRoutes = Router();
 
 const sendPasswordMailController = new SendForgotPasswordMailController();
+const resetPasswordUserController = new ResetPasswordUserController();
 
 // Rota de envio de email para recuperação de senha.
 passwordRoutes.post("/forgot", sendPasswordMailController.handle);
+
+// Rota de reset de senha do usuário.
+passwordRoutes.post("/reset", resetPasswordUserController.handle);
 
 export { passwordRoutes };
