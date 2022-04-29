592fc9f4689f3f193f1780c976d4a79e7e2c8a59
Macmiller <macamagolf@gmail.com>
2022-04-29 00:00:04 -0300

    Criando e implementando funcao findByRefreshToken

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/accounts/infra/typeorm/repositories/UsersTokensRepository.ts
@@ -32,6 +32,11 @@ class UsersTokensRepository implements IUsersTokensRepository {
     async deleteById(id: string): Promise<void> {
         await this.repository.delete(id);
     }
+
+    async findByRefreshToken(refresh_token: string): Promise<UserTokens> {
+        const userToken = await this.repository.findOne({ refresh_token });
+        return userToken;
+    }
 }
 
 export { UsersTokensRepository };
--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -5,6 +5,7 @@ interface IUsersTokensRepository {
     create({ user_id, expires_date, refresh_token }: ICreateUserTokenDTO): Promise<UserTokens>;
     findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UserTokens>;
     deleteById(id: string): Promise<void>;
+    findByRefreshToken(refresh_token: string): Promise<UserTokens>;
 }
 
 export { IUsersTokensRepository };
