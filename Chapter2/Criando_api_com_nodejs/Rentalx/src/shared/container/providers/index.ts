b50e7607fdbdd60a3346d84101751f2df7a4b728
Macmiller <macamagolf@gmail.com>
2022-05-06 00:12:06 -0300

    Criando container StorageProvider

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/index.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
+import { IStorageProvider } from "./StorageProvider/IStoregeProvider";
+import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
 
 container.registerSingleton<IDateProvider>(
     "DayjsDateProvider",
@@ -12,4 +14,9 @@ container.registerSingleton<IDateProvider>(
 container.registerInstance<IMailProvider>(
     "EtherealMailProvider",
     new EtherealMailProvider() // Foi necessário usar o (registerInstance) e instância o (EtherealMailProvider()), para poder conseguir fazer a injeção de dependência no usecase.
+);
+
+container.registerSingleton<IStorageProvider>(
+    "StorageProvider",
+    LocalStorageProvider
 );
