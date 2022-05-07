e8e99cfa209464e190bf56de8685c2c848475947
Macmiller <macamagolf@gmail.com>
2022-05-07 00:07:00 -0300

    Criando container diskStorage

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/index.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/index.ts
@@ -4,6 +4,7 @@ import { IMailProvider } from "./MailProvider/IMailProvider";
 import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { IStorageProvider } from "./StorageProvider/IStoregeProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
 
 container.registerSingleton<IDateProvider>(
@@ -16,7 +17,13 @@ container.registerInstance<IMailProvider>(
     new EtherealMailProvider() // Foi necessário usar o (registerInstance) e instância o (EtherealMailProvider()), para poder conseguir fazer a injeção de dependência no usecase.
 );
 
+// Aqui foi criado essa const para repassar o local do upload dos aquivos.
+const diskStorage = {
+    local: LocalStorageProvider, // local 
+    s3: S3StorageProvider // aws 
+};
+
 container.registerSingleton<IStorageProvider>(
     "StorageProvider",
-    LocalStorageProvider
+    diskStorage[process.env.DISK]
 );
