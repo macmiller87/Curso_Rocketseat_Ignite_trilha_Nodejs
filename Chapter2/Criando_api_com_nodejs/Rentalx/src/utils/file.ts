e1c55ee066ac5bad206a5274eb36b41313ae68bd
Macmiller <macamagolf@gmail.com>
2022-05-07 00:08:00 -0300

    Refatorando arquivo

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/utils/file.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/utils/file.ts
@@ -3,11 +3,10 @@ import fs from "fs"; // Módulo interno do próprio Nodejs
 // Essa cons é responsavel pela verificação do arquivo de upload, se ele existe ou não.
 export const deleteFile = async(filename: string) => {
 
-  try {
-    await fs.promises.stat(filename); // Verica se o arquivo existe pelo nome
-  } catch {
-      return;
-  }
-
-  await fs.promises.unlink(filename); // Se a verificação acima for verdadeira, essa função remove o arquivo duplicado.
-}
+    try {
+        await fs.promises.stat(filename);
+    }catch {
+        return;
+    }
+    await fs.promises.unlink(filename);
+};
