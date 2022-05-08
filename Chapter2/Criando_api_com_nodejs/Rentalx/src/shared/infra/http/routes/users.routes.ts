fcb86e6c85b6a958cfaa9e7591bd0ecd8e6b7aa9
Macmiller <macamagolf@gmail.com>
2022-05-08 17:47:08 -0300

    Criando rota profileUser

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/users.routes.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/infra/http/routes/users.routes.ts
@@ -4,6 +4,7 @@ import uploadConfig from "@config/upload";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUserUseCase/ProfileUserController";
 
 const usersRoutes = Router();
 
@@ -11,11 +12,15 @@ const uploadAvatar = multer(uploadConfig); // Essa const está recebendo, o impo
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserControlle = new ProfileUserController();
 
 // Rota de criação do usuário
 usersRoutes.post("/", createUserController.handle);
 
 // Rota de criação e atualização do avatar do usuário
 usersRoutes.patch("/avatar", ensureAuthenticated, uploadAvatar.single("avatar"), updateUserAvatarController.handle);
+
+// Rota que exibe o perfil do usuário.
+usersRoutes.get("/profile", ensureAuthenticated, profileUserControlle.handle);
 
 export { usersRoutes };
