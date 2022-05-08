414a812113aac1a9bc212d5466b1a4a228d02a46
Macmiller <macamagolf@gmail.com>
2022-05-08 17:44:35 -0300

    Refatorando UploadCarImagesUseCase

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/uploadCarImages/UploadCarImagesUseCase.ts
@@ -1,4 +1,5 @@
 import { ICarsImagesRepository } from "@modules/cars/repositories/ICarsImagesRepository";
+import { IStorageProvider } from "@shared/container/providers/StorageProvider/IStoregeProvider";
 import { inject, injectable } from "tsyringe";
 
 interface IRequest {
@@ -9,12 +10,20 @@ interface IRequest {
 @injectable()
 class UploadCarImagesUseCase {
 
-    constructor(@inject("CarsImagesRepository") private carsImagesRepository: ICarsImagesRepository) {}
+    constructor(
+        @inject("CarsImagesRepository") private carsImagesRepository: ICarsImagesRepository,
+        @inject("StorageProvider") private storageProvider: IStorageProvider
+    ) {}
 
     async execute({ car_id, images_name }: IRequest): Promise<void> {
 
         images_name.map(async (image) => {
+
+            // Salva upload de cars local.
             await this.carsImagesRepository.create(car_id, image);
+
+            // Salva upload de cars via (s3 AWS).
+            await this.storageProvider.save(image, "cars");
         });
     }
 }
