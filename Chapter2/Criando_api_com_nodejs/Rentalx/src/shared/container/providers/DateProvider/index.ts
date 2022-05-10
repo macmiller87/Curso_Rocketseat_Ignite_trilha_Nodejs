71d9b7ff481fa6a48fb508d690a77209f252853e
Macmiller <macamagolf@gmail.com>
2022-05-10 01:01:25 -0300

    Criando container DateProvider

---
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/DateProvider/index.ts
@@ -0,0 +1,8 @@
+import { container } from "tsyringe";
+import { IDateProvider } from "./IDateProvider";
+import { DayjsDateProvider } from "./implementations/DayjsDateProvider";
+
+container.registerSingleton<IDateProvider>(
+    "DayjsDateProvider",
+    DayjsDateProvider
+);
