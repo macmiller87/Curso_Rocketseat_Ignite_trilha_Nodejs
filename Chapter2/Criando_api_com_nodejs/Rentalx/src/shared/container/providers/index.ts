ed1a4a5c38b19a8ce67783d4b430a12a37a26f0a
Macmiller <macamagolf@gmail.com>
2022-05-10 00:59:10 -0300

    refatorando imports dos providers

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/index.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/shared/container/providers/index.ts
@@ -1,29 +1,6 @@
-import { container } from "tsyringe";
-import { IDateProvider } from "./DateProvider/IDateProvider";
-import { IMailProvider } from "./MailProvider/IMailProvider";
-import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvider";
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-import { IStorageProvider } from "./StorageProvider/IStoregeProvider";
-import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
-import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import "./DateProvider";
+import "./MailProvider";
+import "./StorageProvider";
 
-container.registerSingleton<IDateProvider>(
-    "DayjsDateProvider",
-    DayjsDateProvider
-);
 
-container.registerInstance<IMailProvider>(
-    "EtherealMailProvider",
-    new EtherealMailProvider() // Foi necessário usar o (registerInstance) e instância o (EtherealMailProvider()), para poder conseguir fazer a injeção de dependência no usecase.
-);
 
-// Aqui foi criado essa const para repassar o local do upload dos aquivos.
-const diskStorage = {
-    local: LocalStorageProvider, // local 
-    s3: S3StorageProvider // aws 
-};
-
-container.registerSingleton<IStorageProvider>(
-    "StorageProvider",
-    diskStorage[process.env.DISK]
-);
