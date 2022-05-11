fe163d3045bbd3c7c1191a7d45e741dbfae9f105
Macmiller <macamagolf@gmail.com>
2022-05-11 17:43:49 -0300

    Corrigindo testes

--- Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
+++ Chapter2/Criando_api_com_nodejs/Rentalx/src/modules/cars/useCases/listCategories/ListCategoriesController.spec.ts
@@ -35,13 +35,13 @@ describe("Create Category Controller", () => {
             password: "admin"
         });
 
-        const { refresh_token } =  responseToken.body;
+        const { token } =  responseToken.body;
 
         await request(app).post("/categories").send({
             name: "Category Supertest",
             description: "Category Supertest"
         }).set({
-            Authorization: `Bearer ${refresh_token}`
+            Authorization: `Bearer ${token}`
         });
 
         const response = await request(app).get("/categories");
