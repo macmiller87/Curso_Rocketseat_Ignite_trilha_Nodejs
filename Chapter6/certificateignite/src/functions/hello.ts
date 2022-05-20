3a209fa164d84764b36a70de14ad9c189927633c
Macmiller <macamagolf@gmail.com>
2022-05-20 00:01:58 -0300

    Criando estrutura do projeto

---
+++ Chapter6/certificateignite/src/functions/hello.ts
@@ -0,0 +1,11 @@
+import { APIGatewayProxyHandler } from "aws-lambda"
+
+export const handler: APIGatewayProxyHandler = async (event) => {
+
+    return {
+        statusCode: 201,
+        body: JSON.stringify({
+            message: "Hello world Ignite Serverless"
+        }),
+    };
+}; 
