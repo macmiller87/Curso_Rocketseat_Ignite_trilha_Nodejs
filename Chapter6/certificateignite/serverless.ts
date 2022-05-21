e43646a2921bec87e655ff21ca90691942100fc8
Macmiller <macamagolf@gmail.com>
2022-05-21 00:06:25 -0300

    Implementando serverless

--- Chapter6/certificateignite/serverless.ts
+++ Chapter6/certificateignite/serverless.ts
@@ -3,7 +3,11 @@ import type { AWS } from '@serverless/typescript';
 const serverlessConfiguration: AWS = {
   service: 'certificateignite',
   frameworkVersion: '3',
-  plugins: ["serverless-esbuild", "serverless-offline"],
+  plugins: [
+    "serverless-esbuild", 
+    "serverless-dynamodb-local", 
+    "serverless-offline"
+  ],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
@@ -18,13 +22,13 @@ const serverlessConfiguration: AWS = {
   },
   // import the function via paths
   functions: {
-    hello: {
-      handler: "src/functions/hello.handler",
+    generateCertificate: {
+      handler: "src/functions/generateCertificate.handler",
       events: [
         {
           http: {
-            path: "hello",
-            method: "get",
+            path: "generateCertificate",
+            method: "post",
             cors: true
           },
         },
@@ -43,7 +47,43 @@ const serverlessConfiguration: AWS = {
       platform: 'node',
       concurrency: 10,
     },
+    dynamodb: {
+      stages: ["dev", "local"],
+      start: {
+        port: 8000,
+        inMemory: true,
+        migrate: true
+      },
+    },
   },
+
+  // Instruções da tabela que será criada no (Dynamodb na AWS).
+  resources: {
+    Resources: {
+      dbCertificateUsers: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "users_certificate",
+          ProvisionedThroughput: {
+            ReadCapacityUnits: 5,
+            WriteCapacityUnits: 5
+          },
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",
+              AttributeType: "S"
+            }
+          ],
+          KeySchema: [
+            {
+              AttributeName: "id",
+              KeyType: "HASH"
+            }
+          ]
+        }
+      }
+    }
+  }
 };
 
 module.exports = serverlessConfiguration;
